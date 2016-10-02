6c5204ad5cfee27390b8c6c2f5423d3af9e6c542
Algebrah <JWCarroll88@gmail.com>
2016-10-02 16:03:37 -0500

    Initial Commit
    Silly little text adventure game writen in Java

---
+++ src/com/carroll/Location.java
@@ -0,0 +1,35 @@
+package com.carroll;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by jwcar on 10/2/2016.
+ */
+public class Location {
+    private final int locationID;
+    private String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description) {
+        this.locationID = locationID;
+        this.description = description;
+        this.exits = new HashMap<String, Integer>();
+    }
+
+    public void addExit(String direction, int location) {
+        exits.put(direction, location);
+    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<String, Integer>(exits);
+    }
+}
